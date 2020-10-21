2d3f6d283be7e63ab5a23cf754c380256e0cfcf8
Pratham-Mittal <67690275+Pratham-Mittal@users.noreply.github.com>
2020-10-21 09:27:15 +0100

    Add files via upload

---
+++ Lab2/FilledFrame.java
@@ -0,0 +1,36 @@
+package Lab2;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class FilledFrame extends JFrame {
+    int size = 400;
+    public FilledFrame()
+    {
+        JButton butSmall = new JButton("Small");
+        JButton butMedium = new JButton("Medium");
+        JButton butLarge = new JButton("Large");
+        JButton butMessage = new JButton("Say Hi!");
+        SquarePanel panel = new SquarePanel(this);
+        JPanel butPanel = new JPanel();
+        butPanel.add(butSmall);
+        butPanel.add(butMedium);
+        butPanel.add(butLarge);
+        butPanel.add(butMessage);
+        add(butPanel, BorderLayout.NORTH);
+        add(panel, BorderLayout.CENTER);
+        setSize( size+100, size+100 );
+
+        butMessage.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null,"HIII");
+
+            }
+        });
+    }
+
+
+}
---
+++ Lab2/Main.java
@@ -0,0 +1,12 @@
+package Lab2;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	FilledFrame frame = new FilledFrame();
+	frame.setVisible(true);
+    }
+}
